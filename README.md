55500aa1d2bbfdb98641c6cea813b4d2a8341a49
Joshua W <jaxxa90@gmail.com>
2017-12-13 22:15:00 +1100

    Initial Prototype

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ED-WarningOptions
+# ED-Mod
+This is the Readme for the Enhanced Development Mod, ED-ShieldsBasic, for the Game Rimworld.
+
+Additional details can be found in About.xml
+
+#Change Log
+
+01.00.00
+*Initial Release
+
+02.00.00
+*Alpha 14 Update
+
+03.00.00
+*Update to Alpha 15
---
+++ Source/ED-WarningOptions/Detours/Building_Trap.cs
@@ -0,0 +1,40 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace EnhancedDevelopment.Example.Detours
+{
+    [StaticConstructorOnStartup]
+    internal class Main
+    {
+        static Main()
+        {
+            Log.Message("PAtching");
+            HarmonyInstance.Create("EnhancedDevelopment.WarningOptions").PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("PAtching");
+        }
+    }
+
+    [HarmonyPatch(typeof(LetterStack))]
+    [HarmonyPatch("ReceiveLetter")]
+    [HarmonyPatch(new Type[] { typeof(Letter), typeof(string) })]
+    class Patch
+    {
+        static bool Prefix(ref Letter let)
+        {
+            ////AllowGood version only
+            //if (let. == LetterType.Good)
+            //{
+            //    return true;
+            //}
+            return false;
+        }
+    }
+
+}
