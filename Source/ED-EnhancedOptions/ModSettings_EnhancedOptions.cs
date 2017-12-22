f17be8ac5ab2cc6f2ef5314ad26f2d0b9986e68e
Joshua W <jaxxa90@gmail.com>
2017-12-22 19:11:23 +1100

    Updating settings and documentation

--- Source/ED-EnhancedOptions/ModSettings_EnhancedOptions.cs
+++ Source/ED-EnhancedOptions/ModSettings_EnhancedOptions.cs
@@ -57,26 +57,30 @@ namespace EnhancedDevelopment.EnhancedOptions
             listing_Standard.Begin(canvas);
             //            listing_Standard.
             //            listing_Standard.set_ColumnWidth(rect.get_width() - 4f);
+
+            listing_Standard.Label("Sections Starting with '*' only apply after Restart.");
+            listing_Standard.GapLine(12f);
             listing_Standard.Label("Letter Suppression:");
             listing_Standard.Gap(12f);
-            listing_Standard.CheckboxLabeled("Show ThreatBig", ref ShowLettersThreatBig);
-            listing_Standard.CheckboxLabeled("Show ThreatSmall", ref ShowLettersThreatSmall);
-            listing_Standard.CheckboxLabeled("Show NegativeEvent", ref ShowLettersNegativeEvent);
-            listing_Standard.CheckboxLabeled("Show NeutralEvent", ref ShowLettersNeutralEvent);
-            listing_Standard.CheckboxLabeled("Show PositiveEvent", ref ShowLettersPositiveEvent);
-            listing_Standard.CheckboxLabeled("Show ItemStashFeeDemand", ref ShowLettersItemStashFeeDemand);
+            listing_Standard.CheckboxLabeled("Show ThreatBig", ref ShowLettersThreatBig, "True if you want to See Any ThreatBig Letters, False will Hide them. When a Letter is Shown its Name and Type will be written to the Log.");
+            listing_Standard.CheckboxLabeled("Show ThreatSmall", ref ShowLettersThreatSmall, "True if you want to See Any ThreatSmall Letters, False will Hide them. When a Letter is Shown its Name and Type will be written to the Log.");
+            listing_Standard.CheckboxLabeled("Show NegativeEvent", ref ShowLettersNegativeEvent, "True if you want to See Any NegativeEvent Letters, False will Hide them. When a Letter is Shown its Name and Type will be written to the Log.");
+            listing_Standard.CheckboxLabeled("Show NeutralEvent", ref ShowLettersNeutralEvent, "True if you want to See Any NeutralEvent Letters, False will Hide them. When a Letter is Shown its Name and Type will be written to the Log.");
+            listing_Standard.CheckboxLabeled("Show PositiveEvent", ref ShowLettersPositiveEvent, "True if you want to See Any PositiveEvent Letters, False will Hide them. When a Letter is Shown its Name and Type will be written to the Log.");
+            listing_Standard.CheckboxLabeled("Show ItemStashFeeDemand", ref ShowLettersItemStashFeeDemand, "True if you want to See Any ItemStashFeeDemand Letters, False will Hide them. When a Letter is Shown its Name and Type will be written to the Log.");
             listing_Standard.Gap(12f);
-            listing_Standard.CheckboxLabeled("Letter Names To Suppress Enabled", ref LetterNamesToSuppressEnabled);
+            listing_Standard.CheckboxLabeled("Letter Names To Suppress Enabled", ref LetterNamesToSuppressEnabled, "True will Hide any Letters thats Name is in the following List, False to Ignore the List. List is Comma Separated. When a Letter is Shown its Name and Type will be written to the Log.");
             LetterNamesToSuppress = listing_Standard.TextEntry(LetterNamesToSuppress, 2);
 
-            listing_Standard.Gap(12f);
-            listing_Standard.Label("Plant 24H:");
+            listing_Standard.GapLine(12f);
+            
+            listing_Standard.Label("* Plant 24H:");
             listing_Standard.CheckboxLabeled("Plant24H", ref Plant24HEnabled);
-            listing_Standard.Gap(12f);
-            listing_Standard.Label("SafeTrapEnabled:");
+            listing_Standard.GapLine(12f);
+            listing_Standard.Label("* SafeTrapEnabled:");
             listing_Standard.CheckboxLabeled("SafeTrapEnabled", ref SafeTrapEnabled);
-            listing_Standard.Gap(12f);
-            listing_Standard.Label("TurretControlEnabled:");
+            listing_Standard.GapLine(12f);
+            listing_Standard.Label("* TurretControlEnabled:");
             listing_Standard.CheckboxLabeled("TurretControlEnabled", ref TurretControlEnabled);
 
             listing_Standard.End();
