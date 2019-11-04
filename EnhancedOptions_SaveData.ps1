e307be5a60b0c0117851750bad4c8436940dccf6
Jaxxa <jaxxa90@gmail.com>
2019-11-05 07:37:27 +1100

    Adding launcher file

---
+++ EnhancedOptions_SaveData.ps1
@@ -0,0 +1,10 @@
+﻿$RimworldPath = "C:\Games\Rimworld\RimWorld1-0-2408Win64"
+
+$ExePath = $RimworldPath + "\RimWorldWin64.exe"
+$ModDestination = $RimworldPath + "\Mods\ED-EnhancedOptions"
+
+Remove-Item -Path $ModDestination -Recurse
+
+Copy-Item -Path "C:\~Git\Jaxxa-Rimworld\~SubModules\ED-EnhancedOptions\ED-EnhancedOptions" -Destination $ModDestination -Recurse
+
+start -FilePath $ExePath -ArgumentList "-savedatafolder=C:\Games\Rimworld\RimWorld1-0-2408Win64\SaveData"
