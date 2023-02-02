0fba28fbfd1eced40cfcbab47f65634b5df5b40a
Theodore R. Smith <theodore@phpexperts.pro>
2023-02-02 10:11:06 -0600

    Rimworld v1.4 support.

---
+++ Source/ED-EnhancedOptions/1.3/Patches/Patch.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace EnhancedDevelopment.EnhancedOptions
+{
+    abstract class Patch
+    {
+        /// <summary>
+        /// Checks if this Patch should be applied now.
+        /// </summary>
+        /// <returns>Returns true if the Patch should be applied.</returns>
+        protected abstract  bool ShouldPatchApply();
+
+        /// <summary>
+        /// Apply the patch.
+        /// </summary>
+        protected abstract void ApplyPatch(Harmony harmony = null);
+
+        /// <summary>
+        /// The Description of this patch.
+        /// Mainly used for logging.
+        /// </summary>
+        /// <returns>The Patch Description.</returns>
+        protected abstract string PatchDescription();
+
+        /// <summary>
+        /// Checks if this Patch needs to be applied, and applies if needed.
+        /// </summary>
+        public void ApplyPatchIfRequired(Harmony harmony = null)
+        {
+            string _LogLocation = "EnhancedOptions.Patch.ApplyPatchIfRequired: ";
+
+            if (this.ShouldPatchApply())
+            {
+                Patcher.LogMessageIfVerbose(_LogLocation + "Applying Patch: " + this.PatchDescription());
+                this.ApplyPatch(harmony);
+                Patcher.LogMessageIfVerbose(_LogLocation + "Applied Patch: " + this.PatchDescription());
+            }
+            else
+            {
+                Patcher.LogMessageIfVerbose(_LogLocation + "Skipping Applying Patch: " + this.PatchDescription());
+            }
+        }
+        
+
+
+    }
+}
